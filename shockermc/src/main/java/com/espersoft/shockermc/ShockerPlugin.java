0c2dc62144db444bb51ce7726859fd9ace46bddf
Evan Pacini <evan.pacini@gmail.com>
2022-01-17 11:22:42 +0100

    Log when player hit.

--- shockermc/src/main/java/com/espersoft/shockermc/ShockerPlugin.java
+++ shockermc/src/main/java/com/espersoft/shockermc/ShockerPlugin.java
@@ -1,6 +1,9 @@
 package com.espersoft.shockermc;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public class ShockerPlugin extends JavaPlugin {
     @Override
@@ -11,5 +14,12 @@ public class ShockerPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("See you again, SpigotMC!");
+    }
+
+    @EventHandler
+    public void onHit(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player) {
+            getLogger().info("A player was hit!");
+        }
     }
 }
